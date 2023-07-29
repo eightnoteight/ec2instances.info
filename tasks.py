122d4559300cc690ab6a9bf918be664d6dde03ca
Srinivas Devaki <me@eightnoteight.dev>
2023-07-29 23:51:06 +0530

    remove logging error handling, to trigger failure during docker build
    Signed-off-by: Srinivas Devaki <me@eightnoteight.dev>

--- tasks.py
+++ tasks.py
@@ -55,52 +55,32 @@ def build(c):
 def scrape_ec2(c):
     """Scrape EC2 data from AWS and save to local file"""
     ec2_file = "www/instances.json"
-    try:
-        scrape(ec2_file)
-    except Exception as e:
-        print("ERROR: Unable to scrape EC2 data")
-        print(traceback.print_exc())
+    scrape(ec2_file)
 
 
 @task
 def scrape_rds(c):
     """Scrape RDS data from AWS and save to local file"""
     rds_file = "www/rds/instances.json"
-    try:
-        rds_scrape(rds_file)
-    except Exception as e:
-        print("ERROR: Unable to scrape RDS data")
-        print(traceback.print_exc())
+    rds_scrape(rds_file)
 
 
 def scrape_cache(c):
     """Scrape Cache instance data from AWS and save to local file"""
     cache_file = "www/cache/instances.json"
-    try:
-        cache_scrape(cache_file)
-    except Exception as e:
-        print("ERROR: Unable to scrape Cache data")
-        print(traceback.print_exc())
+    cache_scrape(cache_file)
 
 
 def scrape_redshift(c):
     """Scrape Redshift instance data from AWS and save to local file"""
     redshift_file = "www/redshift/instances.json"
-    try:
-        redshift_scrape(redshift_file)
-    except Exception as e:
-        print("ERROR: Unable to scrape Redshift data")
-        print(traceback.print_exc())
+    redshift_scrape(redshift_file)
 
 
 def scrape_opensearch(c):
     """Scrape OpenSearch instance data from AWS and save to local file"""
     opensearch_file = "www/opensearch/instances.json"
-    try:
-        opensearch_scrape(opensearch_file)
-    except Exception as e:
-        print("ERROR: Unable to scrape OpenSearch data")
-        print(traceback.print_exc())
+    opensearch_scrape(opensearch_file)
 
 
 @task
